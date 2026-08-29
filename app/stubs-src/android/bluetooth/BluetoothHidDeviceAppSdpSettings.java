f5b8e7b97620a51b24ca4776278bc2d2ecfe6888
loverofdarkness <loverofdarkness@mail2indian.com>
2026-08-29 14:57:51 +0530

    Add HID SDP compile stub

---
+++ app/stubs-src/android/bluetooth/BluetoothHidDeviceAppSdpSettings.java
@@ -0,0 +1,11 @@
+package android.bluetooth;
+
+/** Compile-time stub for the Android system Bluetooth HID SDP settings API. */
+public class BluetoothHidDeviceAppSdpSettings {
+    public BluetoothHidDeviceAppSdpSettings(
+            String name,
+            String description,
+            String provider,
+            byte subclass,
+            byte[] descriptors) {}
+}
